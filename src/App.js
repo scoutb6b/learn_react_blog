ecdca86438cafeaac60c3a465da3b10cbd98a079
scoutb6b <scoutboq@gmail.com>
2024-07-12 07:05:40 +0900

    tailwindcss 導入

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="mt-2 text-red-500">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
