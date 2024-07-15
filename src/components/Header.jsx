fd20bc8c7b14ef367af51db5b81161a727b68ab0
scoutb6b <scoutboq@gmail.com>
2024-07-15 18:52:30 +0900

    ✨ 課題1_記事一覧ページの作成

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-slate-700 p-4 text-xl font-semibold text-white flex justify-between">
+      <div>Blog</div>
+      <div>お問い合わせ</div>
+    </header>
+  );
+};
+
+export default Header;
